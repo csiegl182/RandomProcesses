bb5555e2e2cbd4b29cfb1e5b93ae715b27a4a282
Christian Siegl <christian.siegl@gmail.com>
2023-02-08 07:43:02 +0100

    add more convenience when plotting acf/ccf

--- plot_phi.m
+++ plot_phi.m
@@ -1,15 +1,21 @@
-function plot_phi(s1, s2, simout, tlim)
+function line = plot_phi(s1, s2, simout, varargin)
+    nargoutchk(0, 1)
 
-Ts = simout.SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
+    Ts = simout.SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
+    
+    x1 = simout.logsout.getElement(s1).Values.Data;
+    x2 = simout.logsout.getElement(s2).Values.Data;
+    
+    [phi, tau] = ccf(x1, x2, Ts);
+    line_element = plot(tau, phi, varargin{:});
 
-x1 = simout.logsout.getElement(s1).Values.Data;
-x2 = simout.logsout.getElement(s2).Values.Data;
+    if nargout == 0
+        grid on
+        xlabel('$\tau\rightarrow$', Interpreter="latex")
+        ylabel(['$\varphi_{', s1, s2, '}(\tau)\rightarrow$'], Interpreter="latex")
+    else
+        line = line_element;
+    end
 
-[phi, tau] = ccf(x1, x2, Ts);
-plot(tau, phi)
-xlim([-tlim, tlim])
-grid on
-xlabel('$\tau\rightarrow$', Interpreter="latex")
-ylabel(['$\varphi_{', s1, s2, '}(\tau)\rightarrow$'], Interpreter="latex")
 end
 
