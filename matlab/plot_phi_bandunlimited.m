b7da5e54017b719a8605aae8a290205393fd2f03
Christian Siegl <christian.siegl@gmail.com>
2023-05-17 07:03:38 +0200

    add function for calculating ccf for bandunlimited signals

---
+++ matlab/plot_phi_bandunlimited.m
@@ -0,0 +1,21 @@
+function line = plot_phi_bandunlimited(s1, s2, result, varargin)
+    nargoutchk(0, 1)
+
+    Ts = result(1).SimulationMetadata.ModelInfo.SolverInfo.FixedStepSize;
+    
+    x1 = get_batch_signal(s1, result);
+    x2 = get_batch_signal(s2, result);
+    
+    [phi, tau] = ccf_batch(x1, x2, Ts);
+    line_element = plot(tau, phi*Ts, varargin{:});
+
+    if nargout == 0
+        grid on
+        xlabel('$\tau\rightarrow$', Interpreter="latex")
+        ylabel(['$\varphi_{', s1, s2, '}(\tau)\rightarrow$'], Interpreter="latex")
+    else
+        line = line_element;
+    end
+
+end
+
